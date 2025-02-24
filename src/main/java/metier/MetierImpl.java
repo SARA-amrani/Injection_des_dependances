1408a12413664a452eac68c71c30d76f31297021
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-02-24 09:54:22 +0000

    une implémentation de IMetier

---
+++ src/main/java/metier/MetierImpl.java
@@ -0,0 +1,26 @@
+package metier;
+
+import dao.IDao;
+
+public class MetierImpl implements IMetier {
+    private IDao dao;
+    // Constructor
+
+    public MetierImpl() {
+    }
+
+    public MetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public double calcul() {
+        double t = dao.getData();
+        double res = t * 23;
+        return res;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
