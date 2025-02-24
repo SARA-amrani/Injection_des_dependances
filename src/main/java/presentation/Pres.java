b56cc13f004eeb961976a952310a03c15533f751
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-02-24 10:13:11 +0000

    l'injection des dépendances : Injection dynamique

--- src/main/java/presentation/Pres.java
+++ src/main/java/presentation/Pres.java
@@ -7,12 +7,10 @@ public class Pres {
     public static void main(String[] args) {
         // injection de dependance en utilisant l'instanciation statique "new"
         DaoImpl dao = new DaoImpl();
-        MetierImpl metier = new MetierImpl();
+        MetierImpl metier = new MetierImpl(); // Injection dynamique
         metier.setDao(dao);
         System.out.println(metier.calcul());
         System.out.println("test");
 
-        // ici notre couche presentation n est pas fermee a la modification
-        // donc obligee de modifier le code source donc ce n est pas bon
     }
 }
